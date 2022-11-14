3b68218550a182da7c38b62504333cc339f0769d
pereira0lucas <lucas12.fp@hotmail.com>
2022-11-14 08:17:20 +0100

    New methods

--- b_preparaTransectas.m
+++ b_preparaTransectas.m
@@ -6,23 +6,54 @@ clear *; close all; clc;
 try init; catch me; cd ..; init; end
 %% ------------------------ CARGANDO LOS DATOS ------------------------
 
-% load("EnsamblesProfiles.mat");
+pltFlag = 1; % quieres graficar?
 
 %% ------------------------- SEPARANDO PERFILES -------------------------
 
-xi=0; yi=0;
-xf=0; yf=2000;
+xi=328803.436; yi=4041814.838;
+xf=329725.341; yf=4041861.742;
 
 dy=100; %[m]
+lenTRS=600; %[m]
 
 RefSL=ReferenceSL;
 RefSL=RefSL.init(xi,yi,xf,yf);
 
 TRS=Transects2;
-TRS=TRS.init(RefSL,dy);
+TRS=TRS.init(RefSL,dy,lenTRS);
 
+%% ------------------------- SEPARANDO PERFILES -------------------------
+
+if pltFlag
+    fig1 = figure; 
+    plot([RefSL.xi RefSL.xf],[RefSL.yi RefSL.yf],'r-',LineWidth=1.2)
+    hold on;
+    for i=1:numel(TRS.xin); plot([TRS.xin(i),TRS.xof(i)],...
+        [TRS.yin(i),TRS.yof(i)],'k-'); 
+    end
+    ax=gca;
+    ax.YLabel.String = 'Y [UTM]';
+    ax.XLabel.String = 'X [UTM]';
+    ax.FontSize=12;
+    ax.FontWeight='bold';
+    legend('Ref. Line', 'Transectos');
+    axis equal; grid; grid minor;
+end
+
+%% ------------------ GIARDANDO UN SHAPEFILE DEL DOMINIO -----------------
+% [ETSG: 3042; ETRS89 / UTM zone 30N (N-E)]
+
+Proj=projcrs(3042);
+
+% [Lat,Lon] = projinv(Proj,[RefSL.xi RefSL.xf],[RefSL.yi RefSL.yf]);
+
+ShapeRef = mapshape([RefSL.xi RefSL.xf],[RefSL.yi RefSL.yf]);
+
+% ShapeRef.ProjectedCRS = Proj;
+% ShapeRef.Filename = 'testRefLine.shp';
 
 %% --- GUARDA DATOS ---
 % save([pathRes 'EnsamblesProfiles.mat'],'ENS')
+shapewrite(ShapeRef,'testRefLine.shp')
 
 %% --------------------------END--------------------------
--- dependencias/Transects2.m
+++ dependencias/Transects2.m
@@ -1,20 +1,37 @@
 classdef  Transects2
     properties
         RefShore
-        xin;
+        xin
+        yin
+        xof
+        yof
     end
     methods
-        function obj = init(obj,Ref,dy)%,lenTRS
+        function obj = init(obj,Ref,dy,LenTRS)%,lenTRS
             obj.RefShore=Ref;
             nTRS=floor(obj.RefShore.len/dy);
 %             res=obj.len-floor(obj.RefShore.len);
 %             a=-1/obj.RefShore.p(1);
-            d=(0:dy:nTRS*dy)';
-            resid = @(x) sqrt((obj.RefShore.xi-x).^2+...
+            dist=(0:dy:nTRS*dy)';
+            resid = @(x) dist - sqrt((obj.RefShore.xi-x).^2+...
                 (obj.RefShore.yi-obj.RefShore.pol1d(x)).^2);
 
-            obj.xin=gmres(resid,d);
-%             obj.yin=
+%             obj.xin=gmres(resid,d);
+
+            obj.xin = fsolve(resid,repmat(obj.RefShore.xi,...
+                [numel(dist),1]));
+            obj.yin = obj.RefShore.pol1d(obj.xin);
+            
+            m = -1/obj.RefShore.p(1);
+            c = obj.RefShore.p(2) - LenTRS * sqrt(obj.RefShore.p(1)^2+1);
+
+            
+            resid2 = @(x) m .* (x - obj.xin) + obj.yin ...
+                - (obj.RefShore.p(1) .* x + c);
+
+            obj.xof = fsolve(resid2,obj.xin);
+            obj.yof = obj.RefShore.p(1) .* obj.xof + c;
+
         end
     end
 end
